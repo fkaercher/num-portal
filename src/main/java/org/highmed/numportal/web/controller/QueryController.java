f779f195039ae4525c2b3c2e45dec3d473d9bf82
Andreas Kling <andreas.kling@bioquant.uni-heidelberg.de>
2024-07-08 12:00:00 +0200

    WIP: search with AQL

---
+++ src/main/java/org/highmed/numportal/web/controller/QueryController.java
@@ -0,0 +1,42 @@
+package org.highmed.numportal.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import org.ehrbase.openehr.sdk.response.dto.QueryResponseData;
+import org.highmed.numportal.domain.dto.QueryDto;
+import org.highmed.numportal.service.ehrbase.EhrBaseService;
+import org.highmed.numportal.service.logger.AuditLog;
+import org.highmed.numportal.web.config.Role;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/query", produces = "application/json")
+@SecurityRequirement(name = "security_auth")
+public class QueryController {
+
+    private final EhrBaseService ehrBaseService;
+
+    @AuditLog
+    @PostMapping("execute")
+    @Operation(description = "Executes an AQL query")
+    @PreAuthorize(Role.MANAGER)
+
+    public ResponseEntity<QueryResponseData> executeManagerProject(
+            @AuthenticationPrincipal @NotNull Jwt principal,
+            @RequestBody @Valid QueryDto queryDto) {
+        return ResponseEntity.ok(
+                ehrBaseService.executePlainQuery(queryDto.getAql()
+                ));
+    }
+}
